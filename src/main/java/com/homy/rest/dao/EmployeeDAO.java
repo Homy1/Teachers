f341163e6be8b5d44ebe81182906daa9cad17274
homy <amirhomayoon.ashrafzadeh@rmit.edu.au>
2022-08-16 15:19:19 +1000

    initial commit

---
+++ src/main/java/com/homy/rest/dao/EmployeeDAO.java
@@ -0,0 +1,28 @@
+package com.homy.rest.dao;
+
+import com.homy.rest.model.Employee;
+import org.springframework.stereotype.Repository;
+
+import com.homy.rest.model.Employees;
+
+@Repository
+public class EmployeeDAO 
+{
+    private static Employees list = new Employees();
+    
+    static 
+    {
+        list.getEmployeeList().add(new Employee(1, "Lokesh", "Gupta", "howtodoinjava@gmail.com"));
+        list.getEmployeeList().add(new Employee(2, "Alex", "Kolenchiskey", "abc@gmail.com"));
+        list.getEmployeeList().add(new Employee(3, "David", "Kameron", "titanic@gmail.com"));
+    }
+    
+    public Employees getAllEmployees() 
+    {
+        return list;
+    }
+    
+    public void addEmployee(Employee employee) {
+        list.getEmployeeList().add(employee);
+    }
+}
